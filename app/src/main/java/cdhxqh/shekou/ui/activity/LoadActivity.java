af00fa64d2295ff12eba47a348fad15a56700eed
taocaocajaini <278233503@qq.com>
2015-10-21 11:09:32 +0800

    loading界面

---
+++ app/src/main/java/cdhxqh/shekou/ui/activity/LoadActivity.java
@@ -0,0 +1,73 @@
+package cdhxqh.shekou.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+
+import cdhxqh.shekou.R;
+
+public class LoadActivity extends BaseActivity {
+
+    private static final int ANIMATION_DURATION = 2000;
+    private static final float SCALE_END = 1.13F;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Handler x = new Handler();
+        x.postDelayed(new splashhandler(), 2000);
+    }
+
+    @Override
+    protected void findViewById() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+
+    class splashhandler implements Runnable {
+
+        public void run() {
+            jumpLoginActivity();
+        }
+
+    }
+
+
+    /**
+     * 跳转至登录界面*
+     */
+    private void jumpLoginActivity() {
+        Intent intent = new Intent();
+        intent.setClass(this, Activity_Login.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_load, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
